84219a2e28440d7b114476a3f4dbf1e328aff0b5
lx kish <urban.ktulhu@gmail.com>
2018-08-05 19:45:01 +1200

    99 Action creators

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from './actions';
+
+class App extends Component {
+
+    componentWillMount() {
+        this.props.movieslist()
+    }
+
+    renderMovies = (movies) => (
+        movies ?
+            movies.map(item => (
+                <div>
+                    {item.name}
+                </div>
+            )) : null
+    )
+
+    render() {
+
+        return (
+            <div>
+                {this.renderMovies(this.props.data.movies)}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        data: state.movies
+    }
+}
+
+export default connect(mapStateToProps, actions)(App);
