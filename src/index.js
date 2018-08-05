2caa744203562bae9a90a6c3c1bc6cf4128fde6c
lx kish <urban.ktulhu@gmail.com>
2018-08-05 13:50:56 +1200

    98 Reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => (<div>Hello, world</div>)
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
+
---
+++ src/reducers/movies_reducer.js
@@ -0,0 +1,11 @@
+
+
+export default function (state = {}, action) {
+
+    switch (action.type) {
+        case 'MOVIES_LIST':
+            return { ...state, movies: action.payload }
+        default:
+            return state;
+    }
+}
